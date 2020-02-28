49f63a1149ea001e6d81b07fb2c0606714417924
AleksaKondricz <al.nikolovska@hotmail.com>
2020-02-28 09:41:41 +0100

    Updates

--- gatsby-browser.js
+++
@@ -1,2 +0,0 @@
-import "./src/styles/global.css"
-import 'antd/dist/antd.css';
--- src/components/button/button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './button.module.css';
-
-
-const Button = ({type, label, disabled}) => {
-return (<button disabled={disabled} className={styles[type]}>{label}</button>  );
-}
- 
-export default Button;
