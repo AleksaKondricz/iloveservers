af73c04c7f0787f574b78221f9eb349bcd17537c
AleksaKondricz <al.nikolovska@hotmail.com>
2020-03-03 16:11:12 +0100

    Hard copy provider made

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
-export { default as Header } from "./header/header";
-export { default as Footer } from "./footer/footer";
-export { default as ProjectCard } from "./projectCard/projectCard";
-export { default as TrophyCard } from "./trophyCard/trophyCard";
-export { default as Form } from "./form/form";
-export { default as Button } from "./button/button";
-export { default as NavSlider } from "./navSlider/navSlider";
+/* eslint-disable import/no-cycle */
+export { default as Header } from "./header/header"
+export { default as Footer } from "./footer/footer"
+export { default as Form } from "./form/form"
+export { default as Button } from "./button/button"
+export { default as NavSlider } from "./navSlider/navSlider"
---
+++ src/constants/offerList.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    id: 1,
+    content: "  System architecture"
+  },
+  { id: 2, content: "  UI/UX\ndesign" },
+  { id: 3, content: " Web / Mobile frontends" },
+  { id: 4, content: " Frontend and Backend logic" },
+  { id: 5, content: " Hosting, deployment" },
+  { id: 6, content: " Guidelines and documentation" },
+  { id: 7, content: " Testing and future maintenance" }
+]
